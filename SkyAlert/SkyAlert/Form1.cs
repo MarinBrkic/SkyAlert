02310cca15001942e68907ee9cdd1f8e1fd3d563
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2025-01-29 09:07:06 +0100

    Add files via upload

---
+++ SkyAlert/SkyAlert/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkyAlert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vremenskaPrognoza vremenskaPrognoza = new vremenskaPrognoza();
+
+            vremenskaPrognoza.Show();
+
+            this.Hide();
+        }
+    }
+}
---
+++ SkyAlert/SkyAlert/vremenskaPrognoza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkyAlert
+{
+    public partial class vremenskaPrognoza : Form
+    {
+        public vremenskaPrognoza()
+        {
+            InitializeComponent();
+        }
+    }
+}
